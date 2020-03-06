aa983c49fd262ac79bef3af319d98e4d97e4353f
Luis Fernando <luisz.dantass@hotmail.com>
2020-03-06 18:08:15 -0300

    Implementação da interface IRepository e classe Repository

---
+++ Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IRepository<Entity>
+    {
+        void Adicionar(Entity entity);
+        IQueryable<Entity> Obter();
+        void Atualizar(Entity entity);
+        void Remover(Entity entity);
+    }
+}
---
+++ Infra/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Interfaces.Repositories;
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Infra.Repositories
+{
+    public class Repository<Entity> : IRepository<Entity>
+        where Entity : class
+    {
+        private readonly ProjectContext _context;
+
+        public Repository(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public void Adicionar(Entity entity)
+        {
+            _context.Set<Entity>().Add(entity);
+        }
+
+        public void Atualizar(Entity entity)
+        {
+            var temp = _context.Entry(entity);
+            temp.State = EntityState.Modified;
+        }
+
+        public IQueryable<Entity> Obter()
+        {
+            return _context.Set<Entity>().AsQueryable<Entity>();
+        }
+
+        public void Remover(Entity entity)
+        {
+            _context.Set<Entity>().Remove(entity);
+        }
+    }
+}

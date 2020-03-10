35a44599a2b0be29beeab628fcc65be43cbd9c68
guilherme <guilhermeteixeira10.gt@gmail.com>
2020-03-09 23:13:27 -0300

    implementando métodos da classe UsuariosController

--- Domain/Controllers/UsuariosController.cs
+++ Domain/Controllers/UsuariosController.cs
@@ -1,5 +1,8 @@
 ﻿using Domain.Interfaces.Uow;
 using System.Web.Mvc;
+using Domain.Entities;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Domain.Controllers
 {
@@ -13,9 +16,35 @@ namespace Domain.Controllers
         }
 
         //Método de Cadastrar(Usuario usuario)
+        public bool Cadastrar(Usuario usuario) {
+            _unitOfWork.RepositoryUsuario.Adicionar(usuario);
+            _unitOfWork.Commit();
+            return true;
+        }
+
         //Método de Autentiar(string email, string senha)
+        public bool Autenticar (string email, string senha) {
+           return _unitOfWork.RepositoryUsuario.Obter().Where(u => u.Email.Equals(email) && u.Senha.Equals(senha)).FirstOrDefault() != null;
+        }
+
         //Método de ObterTodos()
+        public List<Usuario> ObterTodos() {
+            return _unitOfWork.RepositoryUsuario.Obter().ToList();
+        }
+
         //Método de ObterPorId(int id)
+        public Usuario ObterPorId (int id) {
+            return _unitOfWork.RepositoryUsuario.Obter().Where(u => u.Id == id).FirstOrDefault();
+        }
+
         //Método de AlterarSenha(string novaSenha)
+        public void AlterarSenha(int id, string novaSenha) {
+            var usuario = ObterPorId(id);
+            if (usuario != null) {
+                usuario.AlterarSenha(novaSenha);
+                _unitOfWork.RepositoryUsuario.Atualizar(usuario);
+                _unitOfWork.Commit();
+            }
+        }
     }
 }
